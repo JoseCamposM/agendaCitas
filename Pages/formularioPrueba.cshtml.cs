f0e2980c3040fc26c43cbd4ad96aaf221c0796e0
Jose Angel Campos Monsivais <55610980+JoseCamposM@users.noreply.github.com>
2023-10-17 20:22:40 -0600

    inicio
    inicio

---
+++ Pages/formularioPrueba.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class formularioPruebaModel : PageModel
+    {
+        public string time = "15:30pm";
+        public string nameClient = "prueba prueba prueba prueba";
+        public string asunto = "asdadsljn asldnasdka adskndasd knasd knasd knlasd knas dknlasd";
+        public string namePerson = "Lic. Prueba";
+        public string confirmacion = "Si";
+        public string telUser = "824-242-2341";
+        public void OnGet()
+        {
+            
+
+
+
+        }
+    }
+}
