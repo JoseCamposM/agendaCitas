e1da278979d5b472b923615ba390164f6bd47cbf
Jose Angel Campos Monsivais <55610980+JoseCamposM@users.noreply.github.com>
2023-10-18 15:55:19 -0600

    lista formulario

--- Pages/formularioPrueba.cshtml.cs
+++ Pages/formularioPrueba.cshtml.cs
@@ -5,10 +5,27 @@ namespace MyApp.Namespace
 {
     public class formularioPruebaModel : PageModel
     {
-       
-        public string namePerson = "Lic. Prueba";
-        public string confirmacion = "Si";
-        public string telUser = "824-242-2341";
+        public string? nameClient { get; set; }
+        public string? hora { get; set; }
+        public string? asunto { get; set; }
+        public string? asignada { get; set; }
+        public string? confirmacion { get; set; }
+        public string? telefono { get; set; }
+        public string mostrar = "no";
+      
+    public IActionResult OnPost()
+    {
+
+        nameClient = Request.Form["nameClient"];
+        hora = Request.Form["hora"];
+        asunto = Request.Form["asunto"];
+        asignada = Request.Form["asignada"];
+        confirmacion = Request.Form["confirmacion"];
+        telefono = Request.Form["telefono"];
+        mostrar = "si";
+        return Page();
+    }
+
 
         public List<string> nameClientList = new List<string>
             {
@@ -18,7 +35,7 @@ namespace MyApp.Namespace
                 "prueba4"
             };
 
-        public List<string> timeList = new List<string>
+        public List<string> horaList = new List<string>
             {
                 "15:30",
                 "16:00",
@@ -33,6 +50,28 @@ namespace MyApp.Namespace
                 "J zxczxczxc",
                 "AFP nbmbnmbnmnb"
             };
+
+        public List<string> confirmacionList = new List<string>
+            {
+                "si",
+                "no",
+                "no",
+                "si"
+            };
+
+        public List<string> licAtenderaList = new List<string>{
+            "lic prueba1",
+            "lic prueba2",
+            "lic prueba3",
+            "lic prueba4",
+        };
+
+          public List<string> telUserList = new List<string>{
+            "824-002-4555",
+            "824-000-4555",
+            "824-102-0055",
+            "824-102-4500",
+        };
 
         public void OnGet()
         {
