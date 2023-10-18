500fd7a3fcd368b196543a021932e89836a829c1
Jose Angel Campos Monsivais <55610980+JoseCamposM@users.noreply.github.com>
2023-10-17 20:55:44 -0600

    lista algunas

--- Pages/formularioPrueba.cshtml.cs
+++ Pages/formularioPrueba.cshtml.cs
@@ -5,12 +5,35 @@ namespace MyApp.Namespace
 {
     public class formularioPruebaModel : PageModel
     {
-        public string time = "15:30pm";
-        public string nameClient = "prueba prueba prueba prueba";
-        public string asunto = "asdadsljn asldnasdka adskndasd knasd knasd knlasd knas dknlasd";
+       
         public string namePerson = "Lic. Prueba";
         public string confirmacion = "Si";
         public string telUser = "824-242-2341";
+
+        public List<string> nameClientList = new List<string>
+            {
+                "prueba1",
+                "prueba2",
+                "prueba3",
+                "prueba4"
+            };
+
+        public List<string> timeList = new List<string>
+            {
+                "15:30",
+                "16:00",
+                "16:30",
+                "17:00"
+            };
+
+        public List<string> asuntoList = new List<string>
+            {
+                "C-V asdasd as",
+                "D qweqweqweqw" ,
+                "J zxczxczxc",
+                "AFP nbmbnmbnmnb"
+            };
+
         public void OnGet()
         {
             
